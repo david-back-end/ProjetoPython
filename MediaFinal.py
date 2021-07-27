dc4db1ae8a29d0abae740c351452f07e5c8eb41c
David <CostaDavid275@gmail.com>
2021-07-27 09:37:49 -0300

    commmitei

---
+++ MediaFinal.py
@@ -0,0 +1,16 @@
+def resultado_final():
+    aluno = input("Digite seu nome!")
+    print("Seu nome é: " + aluno)
+    nota_1 = int(input("digite sua primeira nota: "))
+    nota_2 = int(input("digite sua segunda nota: "))
+    nota_3 = int(input("digite sua terceira nota: "))
+    nota_4 = int(input("digite sua quarta nota: "))
+    media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+    
+    print("A media do aluno(a) " + aluno + " foi igual a " , media )
+    if media >= 7 :
+        print("Parabéns " + aluno + " você foi aprovado.")
+    else:
+        print("Lamentamos " + aluno + " infelizmente você esta de recuperação.")
+
+resultado_final()
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Este é um projeto de calcular a média final do aluno.
