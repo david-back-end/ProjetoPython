5b23ce02bd4dccf3653af3f9020bfba89825e195
David <CostaDavid275@gmail.com>
2021-07-29 12:25:33 -0300

    commitei

---
+++ Menu.py
@@ -0,0 +1,28 @@
+print( "OP:1 = Abrir LOL - OP:2 = Fechar LOL - OP:3 = Abrir COD - OP:4 = Fechar COD - OP:5 = Sair do menu")
+
+def opcao(menu = 0):
+    menu = int(input("Escreva sua OP: "))
+    if(menu == 1):
+        print("Abrindo o lol...")
+
+    if(menu == 2):
+        print("Fechando o LOL...")
+
+    if(menu == 3):
+        print("Abrindo o COD....")
+
+    if(menu == 4):
+        print("Fechando o COD...")
+
+    if(menu == 5):
+        print("Saindo do menu...")
+
+opcao()
+    
+
+
+
+  
+
+
+
---
+++ Tabuada.py
@@ -0,0 +1,8 @@
+
+limite = 10
+multiplicador = 0
+tabuada = int(input("Qual a tabuada?: "))
+while multiplicador <= limite:
+    resultado = tabuada * multiplicador
+    print( tabuada,"x" , multiplicador,"=", resultado , "\n")
+    multiplicador = multiplicador + 1 
